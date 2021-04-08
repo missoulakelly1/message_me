ea1599a3d660dc6e2fcd1ee7b60473d4b898e4f9
missoulakelly1 <kelly.hanson@swiftcurrentweb.com>
2021-04-08 14:34:48 -0400

    continue authentication -- sessions create & destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,23 @@ class SessionsController < ApplicationController
        
     end
 
+    def create
+        user = user.find_by(username: params[:session][:username])
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id
+            flash[:success] = "You have successfully logged in"
+            redirect_to root_path
+        else
+            flash.now[:error] = "There was something wrong with your login information"
+            render 'login'
+        end
+    end
+
+    def destroy
+        session[:user_id] = nil
+        flash[:success] = "You have successfully logged out"
+        redirect_to login_path
+    end
+
+
 end
