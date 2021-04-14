c0b8b00db1adf36181fc6988e020889426f1bf82
missoulakelly1 <kelly.hanson@swiftcurrentweb.com>
2021-04-14 14:46:52 -0400

    prepare for production

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,8 +12,3 @@ User.create(username: "Arya", password: "password")
 User.create(username: "Frodo", password: "password")
 User.create(username: "Gandalf", password: "password")
 
-Message.create(body: "Ghost, to me!", user_id: user.first )
-Message.create(body: "Thou shalt not pass", user_id: user.second )
-Message.create(body: "Thou shalt not pass", user_id: user.third )
-Message.create(body: "This is the fourth message", user_id:  user.fourth)
-Message.create(body: "Thou shalt not pass", user_id:  user.fifth)
